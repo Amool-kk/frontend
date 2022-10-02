e6bfb75d0fb1ef3cfa85d0770fa749f98eeca3fe
shashank725 <shashank.nov725@gmail.com>
2022-10-02 11:48:14 +0530

    [ADD] People model

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,5 +1,6 @@
-# from django.urls import path
+from django.urls import path
+from .views import PeopleView
 
-# urlpatterns = [
-#     path(''),
-# ]
+urlpatterns = [
+    path('people/', PeopleView, name='people'),
+]
--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import People
+from .serializers import PeopleSerializer
+
 # Create your views here.
+
+
+@api_view(['GET','POST'])
+def PeopleView(request):
+    if request.method == 'GET':
+        people = People.objects.all()
+        serializer = PeopleSerializer(people, many=True)
+        print("SERIALIZER=",serializer.data)
+        return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = PeopleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        print("SERIALIZER=",serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
